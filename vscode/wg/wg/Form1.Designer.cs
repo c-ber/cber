49b03a51e6152d8cd45fa0809a93d0772b856a83
55chenbin <1395329153@qq.com>
2018-01-31 18:28:35 +0800

    jdqs 外挂

---
+++ vscode/wg/wg/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace wg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbox_dir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn_sound = new System.Windows.Forms.Button();
+            this.btn_zero = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbox_dir
+            // 
+            this.tbox_dir.Location = new System.Drawing.Point(96, 12);
+            this.tbox_dir.Name = "tbox_dir";
+            this.tbox_dir.Size = new System.Drawing.Size(457, 25);
+            this.tbox_dir.TabIndex = 0;
+            this.tbox_dir.Text = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\PUBG\\";
+            this.tbox_dir.TextChanged += new System.EventHandler(this.tbox_dir_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(19, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "游戏目录";
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(554, 11);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(41, 28);
+            this.btn_open.TabIndex = 2;
+            this.btn_open.Text = "…";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.groupBox1.Location = new System.Drawing.Point(32, 117);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 147);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "无后座";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(24, 110);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(108, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "变态无后座";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 74);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(108, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "激光无后座";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(108, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "主播无后座";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.groupBox2.Location = new System.Drawing.Point(360, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(293, 147);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "除草";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(54, 84);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(92, 19);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.Text = "地板除草";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(54, 35);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(92, 19);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "简单除草";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn_sound);
+            this.groupBox3.Controls.Add(this.btn_zero);
+            this.groupBox3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.groupBox3.Location = new System.Drawing.Point(32, 315);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(612, 82);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "其他";
+            // 
+            // btn_sound
+            // 
+            this.btn_sound.Location = new System.Drawing.Point(206, 32);
+            this.btn_sound.Name = "btn_sound";
+            this.btn_sound.Size = new System.Drawing.Size(116, 33);
+            this.btn_sound.TabIndex = 1;
+            this.btn_sound.Text = "声音优化";
+            this.btn_sound.UseVisualStyleBackColor = true;
+            // 
+            // btn_zero
+            // 
+            this.btn_zero.Location = new System.Drawing.Point(33, 32);
+            this.btn_zero.Name = "btn_zero";
+            this.btn_zero.Size = new System.Drawing.Size(116, 33);
+            this.btn_zero.TabIndex = 0;
+            this.btn_zero.Text = "零秒打药";
+            this.btn_zero.UseVisualStyleBackColor = true;
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(155, 430);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(115, 36);
+            this.btn_start.TabIndex = 6;
+            this.btn_start.Text = "开启";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(360, 430);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(117, 36);
+            this.btn_close.TabIndex = 7;
+            this.btn_close.Text = "清理";
+            this.btn_close.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 501);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbox_dir);
+            this.Name = "Form1";
+            this.Text = "cber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbox_dir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button btn_sound;
+        private System.Windows.Forms.Button btn_zero;
+    }
+}
+
---
+++ vscode/wg/wg/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string game_dir = "";
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fold = new FolderBrowserDialog();
+            if (fold.ShowDialog() == DialogResult.OK)
+            {
+                tbox_dir.Text = fold.SelectedPath;
+                game_dir = tbox_dir.Text;
+            }
+        }
+
+        private void tbox_dir_TextChanged(object sender, EventArgs e)
+        {
+            game_dir = tbox_dir.Text;
+        }
+
+        public static void CopyFolder(string strFromPath, string strToPath)
+        {
+            //如果源文件夹不存在，则创建
+            if (!Directory.Exists(strFromPath))
+            {
+                Directory.CreateDirectory(strFromPath);
+            }
+            //取得要拷贝的文件夹名
+            string strFolderName = strFromPath.Substring(strFromPath.LastIndexOf("\\") +
+              1, strFromPath.Length - strFromPath.LastIndexOf("\\") - 1);
+            //如果目标文件夹中没有源文件夹则在目标文件夹中创建源文件夹
+            if (!Directory.Exists(strToPath + "\\" + strFolderName))
+            {
+                Directory.CreateDirectory(strToPath + "\\" + strFolderName);
+            }
+            //创建数组保存源文件夹下的文件名
+            string[] strFiles = Directory.GetFiles(strFromPath);
+            //循环拷贝文件
+            for (int i = 0; i < strFiles.Length; i++)
+            {
+                //取得拷贝的文件名，只取文件名，地址截掉。
+                string strFileName = strFiles[i].Substring(strFiles[i].LastIndexOf("\\") + 1, strFiles[i].Length - strFiles[i].LastIndexOf("\\") - 1);
+                //开始拷贝文件,true表示覆盖同名文件
+                File.Copy(strFiles[i], strToPath + "\\" + strFolderName + "\\" + strFileName, true);
+            }
+            //创建DirectoryInfo实例
+            DirectoryInfo dirInfo = new DirectoryInfo(strFromPath);
+            //取得源文件夹下的所有子文件夹名称
+            DirectoryInfo[] ZiPath = dirInfo.GetDirectories();
+            for (int j = 0; j < ZiPath.Length; j++)
+            {
+                //获取所有子文件夹名
+                string strZiPath = strFromPath + "\\" + ZiPath[j].ToString();
+                //把得到的子文件夹当成新的源文件夹，从头开始新一轮的拷贝
+                CopyFolder(strZiPath, strToPath + "\\" + strFolderName);
+            }
+        }
+
+        public static void exec_cmd(string cmd)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/c C:\\Windows\\System32\\cmd.exe";
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+            startInfo.Verb = "RunAs";
+            Process p = new Process();
+            p.StartInfo = startInfo;
+            p.Start();
+
+            //向cmd窗口发送输入信息
+            p.StandardInput.WriteLine(cmd);
+            p.StandardInput.WriteLine("exit");
+            p.StandardInput.AutoFlush = true;
+
+            //获取输出信息
+            string strOuput = p.StandardOutput.ReadToEnd();
+            string strError = p.StandardError.ReadToEnd();
+            //等待程序执行完退出进程
+            p.WaitForExit();
+            p.Close();
+        }
+        
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            //第一步，复制paks文件为Maks,复制后尽量做一个校验
+            string pak_dir = game_dir + @"TslGame\Content\Paks\";
+            string bak_dir = game_dir + @"TslGame\Content\Maks\";
+            string bak_dir_1 = game_dir + @"TslGame\Content\Caks\";
+
+            if(Directory.Exists("bak_dir"))
+            {
+                MessageBox.Show("备份目录已经有了，请处理");
+                return;
+            }
+            CopyFolder(pak_dir, bak_dir);
+
+            //第二步，将paks重命名为Caks
+            DirectoryInfo di = new DirectoryInfo(pak_dir);
+            di.MoveTo(bak_dir_1);
+
+            //第三步，做文件软链接, 第一个脚本
+            string wg_pak = @"E:\share\cber\vscode\wg\wg\pak\";
+            string cmd_head = "mklink \"" + bak_dir + "TslGame-WindowsNoEditor_uicb1.pak\"  \"" + wg_pak + "主播无后.pak\"";
+            exec_cmd(cmd_head);
+
+            cmd_head = "mklink \"" + bak_dir + "TslGame-WindowsNoEditor_uicb2.pak\"  \"" + wg_pak + "草皮除草.pak\"";
+            exec_cmd(cmd_head);
+
+            cmd_head = "mklink \"" + bak_dir + "TslGame-WindowsNoEditor_uicb3.pak\"  \"" + wg_pak + "神仙吃药.pak\"";
+            exec_cmd(cmd_head);
+
+            //第四步，做目录软链接，第二个脚本
+            cmd_head = "mklink /j \"" + pak_dir + "\"  \""+ bak_dir + "\"";
+            exec_cmd(cmd_head);
+            MessageBox.Show("提示", "功能正常开启");
+         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            game_dir = tbox_dir.Text;
+        }
+    }
+    /// <summary>
+    /// 后座类型，主播，激光，变态
+    /// </summary>
+    enum HOUZUO
+    {
+        HOUZUO_ZHUBO = 0,
+        HOUZUO_JIGUANG = 1,
+        HOUZUO_BIANTAI = 2
+    };
+
+    /// <summary>
+    /// 除草类型，简单，地板
+    /// </summary>
+    enum CHUCAO
+    {
+        CHUCAO_SIMPLE = 0,
+        CHUCAO_FLOOR = 1
+    };
+
+    public class JUEDI
+    {
+        HOUZUO hz = HOUZUO.HOUZUO_ZHUBO;
+        CHUCAO cc = CHUCAO.CHUCAO_SIMPLE;
+        bool zero_medic = true;
+        JUEDI()
+        {
+
+        }
+    }
+}
