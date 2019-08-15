b96458fa17c461eb1ca9c5b708988b651fc968db
55chenbin <1395329153@qq.com>
2019-08-15 22:50:10 +0800

    文件名伪装

---
+++ vscode/vsharp/命名伪装/命名伪装/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 命名伪装
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = System.Windows.Forms.Application.StartupPath;
+            DirectoryInfo folder = new DirectoryInfo(path);
+            FileInfo[] files = folder.GetFiles("*",SearchOption.AllDirectories);
+            foreach (FileInfo fi in files)
+            {
+                if (!fi.Name.Contains(".exe"))
+                {
+                    File.Move(fi.Name, fi.Name + ".jpg");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = System.Windows.Forms.Application.StartupPath;
+            DirectoryInfo folder = new DirectoryInfo(path);
+            FileInfo[] files = folder.GetFiles("*", SearchOption.AllDirectories);
+            foreach (FileInfo fi in files)
+            {
+                if (!fi.Name.Contains(".exe"))
+                {
+                    File.Move(fi.Name, fi.Name.Replace(".jpg", ""));
+                }
+            }
+        }
+    }
+}
