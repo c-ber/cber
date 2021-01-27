d86a04446370d03965d4ed37859ea0f83190d7e4
55chenbin <1395329153@qq.com>
2021-01-27 19:51:20 +0800

    1、增加了读取进程内存数据，并保存到文件中

---
+++ vscode/cotool/cotool/crack/Form_crack.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace cotool.crack
+{
+    partial class Form_crack
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_getmem = new System.Windows.Forms.Button();
+            this.tbox_len = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbox_addr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbox_pro = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_getmem);
+            this.groupBox1.Controls.Add(this.tbox_len);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbox_addr);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbox_pro);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "内存到文件";
+            // 
+            // btn_getmem
+            // 
+            this.btn_getmem.Location = new System.Drawing.Point(251, 19);
+            this.btn_getmem.Name = "btn_getmem";
+            this.btn_getmem.Size = new System.Drawing.Size(57, 59);
+            this.btn_getmem.TabIndex = 6;
+            this.btn_getmem.Text = "开始";
+            this.btn_getmem.UseVisualStyleBackColor = true;
+            this.btn_getmem.Click += new System.EventHandler(this.btn_getmem_Click);
+            // 
+            // tbox_len
+            // 
+            this.tbox_len.Location = new System.Drawing.Point(173, 57);
+            this.tbox_len.Name = "tbox_len";
+            this.tbox_len.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tbox_len.Size = new System.Drawing.Size(72, 21);
+            this.tbox_len.TabIndex = 5;
+            this.tbox_len.Text = "4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "长度：";
+            // 
+            // tbox_addr
+            // 
+            this.tbox_addr.Location = new System.Drawing.Point(47, 57);
+            this.tbox_addr.Name = "tbox_addr";
+            this.tbox_addr.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tbox_addr.Size = new System.Drawing.Size(72, 21);
+            this.tbox_addr.TabIndex = 3;
+            this.tbox_addr.Text = "0x008e4000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "地址：";
+            // 
+            // cbox_pro
+            // 
+            this.cbox_pro.FormattingEnabled = true;
+            this.cbox_pro.Location = new System.Drawing.Point(47, 20);
+            this.cbox_pro.Name = "cbox_pro";
+            this.cbox_pro.Size = new System.Drawing.Size(198, 20);
+            this.cbox_pro.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "进程：";
+            // 
+            // Form_crack
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form_crack";
+            this.Size = new System.Drawing.Size(796, 416);
+            this.Load += new System.EventHandler(this.Form_crack_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbox_pro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbox_addr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbox_len;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_getmem;
+    }
+}
---
+++ vscode/cotool/cotool/crack/Form_crack.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+namespace cotool.crack
+{
+    public partial class Form_crack : UserControl
+    {
+        public Form_crack()
+        {
+            InitializeComponent();
+        }
+
+        //根据进程名称获取PID
+        public static int GetPidByProcessName(string processName)
+        {
+            Process[] arrayProcess = Process.GetProcessesByName(processName);
+            foreach (Process p in arrayProcess)
+            {
+                return p.Id;
+            }
+            return 0;
+        }
+
+        private void Form_crack_Load(object sender, EventArgs e)
+        {
+            DataTable dtcomProcessName = new DataTable();
+            dtcomProcessName.Columns.Add("name");
+            dtcomProcessName.Columns.Add("pid");
+
+            Process[] ps = Process.GetProcesses();//遍历所有的进程
+            foreach (Process p in ps.OrderBy(m => m.ProcessName))
+            {
+                DataRow row = dtcomProcessName.NewRow();
+                row["name"] = p.ProcessName;
+                row["pid"] = p.Id;
+                dtcomProcessName.Rows.Add(row);
+                dtcomProcessName.AcceptChanges();
+            }
+
+            cbox_pro.DataSource = dtcomProcessName;
+            cbox_pro.DisplayMember = "name";
+            cbox_pro.ValueMember = "pid";
+        }
+
+        #region API
+
+        //从指定内存中读取字节集数据
+        [DllImportAttribute("kernel32.dll", EntryPoint = "ReadProcessMemory")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, int nSize, IntPtr lpNumberOfBytesRead);
+
+        //从指定内存中写入字节集数据
+        [DllImportAttribute("kernel32.dll", EntryPoint = "WriteProcessMemory")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, int[] lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
+
+        //打开一个已存在的进程对象，并返回进程的句柄
+        [DllImportAttribute("kernel32.dll", EntryPoint = "OpenProcess")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        //关闭一个内核对象。其中包括文件、文件映射、进程、线程、安全和同步对象等。
+        [DllImport("kernel32.dll")]
+        private static extern void CloseHandle(IntPtr hObject);
+
+        #endregion
+
+        #region 使用方法
+
+        //读取内存中的一段数据
+        public static byte[] ReadMemoryValue(int baseAddress, string processName, int buf_size)
+        {
+            Byte[] ret_aray = new Byte[buf_size];
+            try
+            {
+                byte[] buffer = new byte[4];
+                //获取缓冲区地址
+                IntPtr byteAddress = Marshal.UnsafeAddrOfPinnedArrayElement(buffer, 0);
+                //打开一个已存在的进程对象  0x1F0FFF 最高权限
+                IntPtr hProcess = OpenProcess(0x1F0FFF, false, GetPidByProcessName(processName));
+                //将指定内存中的值读入缓冲区：进程句柄，读取地址，接收地址，读取字节数，实际传送的字节数
+                ReadProcessMemory(hProcess, (IntPtr)baseAddress, byteAddress, buf_size, IntPtr.Zero);
+                //关闭操作
+                CloseHandle(hProcess);
+
+                //从非托管内存中读取整个字节buf。
+                for (int i = 0; i < buf_size; i++)
+                {
+                    ret_aray[i] = Marshal.ReadByte(byteAddress, i);
+                }
+                return ret_aray;
+            }
+            catch
+            {
+                return ret_aray;
+            }
+        }
+
+        //读取内存中的值
+        public static int ReadMemoryValue(int baseAddress, string processName)
+        {
+            try
+            {
+                byte[] buffer = new byte[4];
+                //获取缓冲区地址
+                IntPtr byteAddress = Marshal.UnsafeAddrOfPinnedArrayElement(buffer, 0);
+                //打开一个已存在的进程对象  0x1F0FFF 最高权限
+                IntPtr hProcess = OpenProcess(0x1F0FFF, false, GetPidByProcessName(processName));
+                //将指定内存中的值读入缓冲区
+                ReadProcessMemory(hProcess, (IntPtr)baseAddress, byteAddress, 4, IntPtr.Zero);
+                //关闭操作
+                CloseHandle(hProcess);
+                //从非托管内存中读取一个 32 位带符号整数。
+                return Marshal.ReadInt32(byteAddress);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        //将值写入指定内存地址中
+        public static void WriteMemoryValue(int baseAddress, string processName, int value)
+        {
+            try
+            {
+                //打开一个已存在的进程对象  0x1F0FFF 最高权限
+                IntPtr hProcess = OpenProcess(0x1F0FFF, false, GetPidByProcessName(processName));
+                //从指定内存中写入字节集数据
+                WriteProcessMemory(hProcess, (IntPtr)baseAddress, new int[] { value }, 4, IntPtr.Zero);
+                //关闭操作
+                CloseHandle(hProcess);
+            }
+            catch { }
+        }
+
+        #endregion
+
+        public int StringToHexOrDec(string strData)
+        {
+            int dData = -1;
+            try
+            {
+                if ((strData.Length > 2))
+                {
+                    if ((strData.Substring(0, 2).Equals("0x")) || (strData.Substring(0, 2).Equals("0X")))
+                    {
+                        string str_sub = strData.Substring(2, strData.Length - 2);
+                        dData = int.Parse(str_sub, System.Globalization.NumberStyles.HexNumber);
+                    }
+                    else
+                    {
+                        dData = int.Parse(strData, System.Globalization.NumberStyles.Integer);
+                    }
+                }
+                else
+                {
+                    dData = int.Parse(strData, System.Globalization.NumberStyles.Integer);
+                }
+            }
+            catch (Exception)
+            {
+                //MessageBox.Show("输入错误: " + strData, "错误");
+            }
+            return dData;
+        }
+
+
+        private void btn_getmem_Click(object sender, EventArgs e)
+        {
+            string path = "mem.txt";
+            Byte[] array;
+            int baseAddress = StringToHexOrDec(tbox_addr.Text);
+            int buf_size = Int32.Parse(tbox_len.Text);
+
+            try
+            {
+                array = ReadMemoryValue(baseAddress, cbox_pro.Text, buf_size);
+
+
+                //创建一个文件流
+                FileStream fs = new FileStream(path, FileMode.Create);
+
+                //将byte数组写入文件中
+                fs.Write(array, 0, array.Length);
+                //所有流类型都要关闭流，否则会出现内存泄露问题
+                fs.Close();
+                MessageBox.Show("保存文件成功");
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("读取内存并保存文件失败");
+            }
+        }
+    }
+}
