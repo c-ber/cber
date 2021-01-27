f3b126722863e375fcc791639595846f8b463185
55chenbin <1395329153@qq.com>
2021-01-27 19:50:11 +0800

    增加考试模块，但是没啥内容

---
+++ vscode/cotool/cotool/exam/Form_ks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cotool.exam
+{
+    public partial class Form_ks : UserControl
+    {
+        public Form_ks()
+        {
+            InitializeComponent();
+        }
+    }
+}
