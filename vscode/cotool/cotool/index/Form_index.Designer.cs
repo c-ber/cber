5914b856df1892a09ef2ae9163fc2c561c3fbb00
55chenbin <1395329153@qq.com>
2021-10-19 07:15:01 +0800

    1、优化首页功能，增加数据库访问

---
+++ vscode/cotool/cotool/index/Form_index.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace cotool.index
+{
+    partial class Form_index
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbox_main = new System.Windows.Forms.RichTextBox();
+            this.btn_savetext = new System.Windows.Forms.Button();
+            this.rtbox_content = new System.Windows.Forms.RichTextBox();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_savedb = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbox_main
+            // 
+            this.rtbox_main.BackColor = System.Drawing.Color.AliceBlue;
+            this.rtbox_main.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbox_main.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rtbox_main.ForeColor = System.Drawing.Color.Red;
+            this.rtbox_main.Location = new System.Drawing.Point(250, 42);
+            this.rtbox_main.Name = "rtbox_main";
+            this.rtbox_main.Size = new System.Drawing.Size(295, 55);
+            this.rtbox_main.TabIndex = 0;
+            this.rtbox_main.Text = "";
+            // 
+            // btn_savetext
+            // 
+            this.btn_savetext.Location = new System.Drawing.Point(605, 366);
+            this.btn_savetext.Name = "btn_savetext";
+            this.btn_savetext.Size = new System.Drawing.Size(75, 23);
+            this.btn_savetext.TabIndex = 1;
+            this.btn_savetext.Text = "保存";
+            this.btn_savetext.UseVisualStyleBackColor = true;
+            this.btn_savetext.Click += new System.EventHandler(this.btn_savetext_Click);
+            // 
+            // rtbox_content
+            // 
+            this.rtbox_content.BackColor = System.Drawing.Color.AliceBlue;
+            this.rtbox_content.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbox_content.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rtbox_content.ForeColor = System.Drawing.Color.Red;
+            this.rtbox_content.Location = new System.Drawing.Point(89, 114);
+            this.rtbox_content.Name = "rtbox_content";
+            this.rtbox_content.Size = new System.Drawing.Size(649, 220);
+            this.rtbox_content.TabIndex = 2;
+            this.rtbox_content.Text = "";
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(89, 366);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(75, 23);
+            this.btn_load.TabIndex = 3;
+            this.btn_load.Text = "加载";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // btn_savedb
+            // 
+            this.btn_savedb.Location = new System.Drawing.Point(696, 366);
+            this.btn_savedb.Name = "btn_savedb";
+            this.btn_savedb.Size = new System.Drawing.Size(75, 23);
+            this.btn_savedb.TabIndex = 4;
+            this.btn_savedb.Text = "存数据库";
+            this.btn_savedb.UseVisualStyleBackColor = true;
+            this.btn_savedb.Click += new System.EventHandler(this.btn_savedb_Click);
+            // 
+            // Form_index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_savedb);
+            this.Controls.Add(this.btn_load);
+            this.Controls.Add(this.rtbox_content);
+            this.Controls.Add(this.btn_savetext);
+            this.Controls.Add(this.rtbox_main);
+            this.Name = "Form_index";
+            this.Size = new System.Drawing.Size(796, 416);
+            this.Load += new System.EventHandler(this.Form_index_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbox_main;
+        private System.Windows.Forms.Button btn_savetext;
+        private System.Windows.Forms.RichTextBox rtbox_content;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_savedb;
+    }
+}
---
+++ vscode/cotool/cotool/index/Form_index.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using cotool.sysset;
+using cotool.game;
+using System.IO;
+using cotool.DirFile;
+using cotool.crack;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+
+namespace cotool.index
+{
+    public partial class Form_index : UserControl
+    {
+        public Form_index()
+        {
+            InitializeComponent();
+        }
+
+        string db_tablename = "t_everyday";
+        Mysql mysql = null; //数据库连接
+        DataTable db_table = new DataTable();//建立个数据表
+
+
+        void InsertaData_EveryDay()
+        {
+            int tb_id = 0;//没有记录的话都是返回null，也不报错，没关系,初始为0就好了
+            get_db_max_id(ref tb_id, "tb_id");
+            tb_id = tb_id + 1;
+
+            System.IO.MemoryStream mstream = new System.IO.MemoryStream();
+            rtbox_content.SaveFile(mstream, RichTextBoxStreamType.RichText);
+            //将流转换成数组
+            byte[] bWrite = mstream.ToArray();
+
+            try
+            {
+                mysql = new Mysql(Mysql.connectstring);
+                MySqlParameter[] para ={
+                    Mysql.MakeInParam("@id", MySqlDbType.Int32, tb_id),
+                    Mysql.MakeInParam("@date", MySqlDbType.DateTime, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")),
+                    Mysql.MakeInParam("@main", MySqlDbType.String, rtbox_main.Text),
+                    Mysql.MakeInParam("@content", MySqlDbType.String, rtbox_content.Text),
+                    Mysql.MakeInParam("@rtf", MySqlDbType.VarBinary, bWrite) };
+
+                string sql = "INSERT INTO `" + db_tablename + "` VALUES (@id, @date, @main,@content,@rtf)";
+                MyLog.Log("记录everyday", sql);
+                mysql.ExecuteNonQuery(sql, para);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mysql.Dispose();
+            }
+        }
+
+        string file_index = "index.txt";
+        string file_content = "content.txt";
+        private void btn_savetext_Click(object sender, EventArgs e)
+        {
+            
+            if ((System.IO.Path.GetExtension(file_index)).ToLower() == ".txt")
+                rtbox_main.SaveFile(file_index, RichTextBoxStreamType.PlainText);
+            else
+                rtbox_main.SaveFile(file_index);
+
+            if ((System.IO.Path.GetExtension(file_content)).ToLower() == ".txt")
+                rtbox_content.SaveFile(file_content, RichTextBoxStreamType.PlainText);
+            else
+                rtbox_content.SaveFile(file_content);
+        }
+
+        private void Form_index_Load(object sender, EventArgs e)
+        {
+            //设置标题
+            if(!File.Exists(file_index))
+            {
+                rtbox_main.Text = "乐观又不失去对客观环境的平静认知";
+                return;
+            }
+            if ((System.IO.Path.GetExtension(file_index)).ToLower() == ".txt")
+                rtbox_main.LoadFile(file_index, RichTextBoxStreamType.PlainText);
+            else
+                rtbox_main.LoadFile(file_index);
+
+            //设置内容
+            if (!File.Exists(file_content))
+            {
+                return;
+            }
+            if ((System.IO.Path.GetExtension(file_content)).ToLower() == ".txt")
+                rtbox_content.LoadFile(file_content, RichTextBoxStreamType.PlainText);
+            else
+                rtbox_content.LoadFile(file_content);
+        }
+
+        /// <summary>
+        /// 这个函数是原封不动的拷贝真三界面的，考虑公用
+        /// </summary>
+        /// <param name="max_id"></param>
+        /// <param name="id_type"></param>
+        void get_db_max_id(ref int max_id, string id_type)
+        {
+            mysql = new Mysql(Mysql.connectstring);
+
+            string sql = "select max(" + id_type + ") from " + db_tablename;
+            try
+            {
+                db_table = mysql.ExecuteDataTable(db_tablename, sql);
+                if (db_table == null) return;
+                if (db_table.Rows.Count <= 0)
+                {
+                    db_table = null;
+                    return;
+                }
+                for (int i = 0; i < db_table.Rows.Count; i++)
+                {
+                    max_id = Int32.Parse(db_table.Rows[i][0].ToString().Trim());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mysql.Dispose();
+            }
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            int tb_id = 0;//没有记录的话都是返回null，也不报错，没关系,初始为0就好了
+            get_db_max_id(ref tb_id, "tb_id");
+            mysql = new Mysql(Mysql.connectstring);
+
+            string sql = "select * from " + db_tablename + " where tb_id = " + tb_id.ToString();
+            try
+            {
+                db_table = mysql.ExecuteDataTable(db_tablename, sql);
+                if (db_table == null) return;
+                if (db_table.Rows.Count <= 0)
+                {
+                    db_table = null;
+                    return;
+                }
+                rtbox_main.Text = db_table.Rows[0][2].ToString().Trim();
+
+                byte[] bWrite = Encoding.UTF8.GetBytes(db_table.Rows[0][4].ToString());
+                //将数组转换成stream
+                MemoryStream mstream = new MemoryStream(bWrite, false);
+                //将stream填充到RichTextBox
+                rtbox_content.LoadFile(mstream, RichTextBoxStreamType.RichText);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                mysql.Dispose();
+            }
+        }
+
+        private void btn_savedb_Click(object sender, EventArgs e)
+        {
+            InsertaData_EveryDay();
+        }
+    }
+}
